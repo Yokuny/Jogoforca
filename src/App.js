23c37198763ab85545f02bf43e41f319730bc499
yokuny <felipe.vni@hotmail.com>
2023-02-15 10:00:23 -0300

    all project template

--- src/App.js
+++ src/App.js
@@ -1,3 +1,32 @@
+import palavras from "./palavras";
+const alfabeto = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 function App() {
   return <></>;
 }
